36595b2a179c6d593709b2c7be974bf55b4a1798
clive <francoclivemaleke23@gmail.com>
2020-08-12 22:40:49 +0700

    Updates

---
+++ precache-manifest.d867ceaa4c704d93b1cf93a4c687e52a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "933e4beb362337dd647d75e4a5f5b7f4",
+    "url": "/hello-clive/index.html"
+  },
+  {
+    "revision": "183066926276d9185a5f",
+    "url": "/hello-clive/static/css/main.11f41e8e.chunk.css"
+  },
+  {
+    "revision": "6512db94dde396e080ea",
+    "url": "/hello-clive/static/js/2.a7291ffb.chunk.js"
+  },
+  {
+    "revision": "b7bdffd4fd4ff576b1a1d037e48d8a27",
+    "url": "/hello-clive/static/js/2.a7291ffb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "183066926276d9185a5f",
+    "url": "/hello-clive/static/js/main.e3b728bc.chunk.js"
+  },
+  {
+    "revision": "ad804ceb02bb3fb2b5ff",
+    "url": "/hello-clive/static/js/runtime-main.63ff5091.js"
+  }
+]);
