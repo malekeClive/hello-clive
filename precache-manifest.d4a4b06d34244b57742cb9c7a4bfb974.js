a49f0911e34b0fc8b9325a37fb3ba2327459346f
clive <francoclivemaleke23@gmail.com>
2020-11-27 22:16:32 +0700

    resolved

---
+++ precache-manifest.d4a4b06d34244b57742cb9c7a4bfb974.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b0f9777e3fe997abf7a71f6dc42e97c",
+    "url": "/hello-clive/index.html"
+  },
+  {
+    "revision": "a10b871c6e878754fbb6",
+    "url": "/hello-clive/static/css/main.92f363ab.chunk.css"
+  },
+  {
+    "revision": "eb71105c93094e6e033e",
+    "url": "/hello-clive/static/js/2.48b71ef6.chunk.js"
+  },
+  {
+    "revision": "2206f6f6cbbf99cae08d01b2952f71ad",
+    "url": "/hello-clive/static/js/2.48b71ef6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a10b871c6e878754fbb6",
+    "url": "/hello-clive/static/js/main.b9684cda.chunk.js"
+  },
+  {
+    "revision": "ad804ceb02bb3fb2b5ff",
+    "url": "/hello-clive/static/js/runtime-main.63ff5091.js"
+  }
+]);
